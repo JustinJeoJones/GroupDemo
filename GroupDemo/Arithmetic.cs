25692c4f9cb2d5aca698e372a35b5a953da2b1ae
Justin <Justin@grandcircus.co>
2024-05-14 13:21:40 -0400

    Added arithmetic class

---
+++ GroupDemo/Arithmetic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupDemo
+{
+    public class Arithmetic
+    {
+        public static int Adder(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Subber(int x, int y)
+        {
+            return x - y;
+        }
+        public static int Multiplier(int x, int y)
+        {
+            return x * y;
+        }
+        public static int Divider(int x, int y)
+        {
+            return x % y;
+        }
+    }
+}
